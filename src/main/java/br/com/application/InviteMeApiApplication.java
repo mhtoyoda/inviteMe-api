adea7b347ca5218693ba3834d60b4815728e3738
Marcelo Toyoda <marcelohtoy@gmail.com>
2016-05-04 00:19:02 -0300

    Estrutura inicial da API InviteMe

---
+++ src/main/java/br/com/application/InviteMeApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InviteMeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InviteMeApiApplication.class, args);
+	}
+}
