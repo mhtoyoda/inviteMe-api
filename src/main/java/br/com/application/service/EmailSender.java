1d12d74c00bc454d618551bdb6ebe48d810f2386
Marcelo Toyoda <marcelohtoy@gmail.com>
2016-05-09 21:07:34 -0300

    Configuração para envio de emails

---
+++ src/main/java/br/com/application/service/EmailSender.java
@@ -0,0 +1,34 @@
+package br.com.application.service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSender {
+
+	private Logger logger = Logger.getLogger(EmailSender.class);
+	
+	@Autowired
+	private JavaMailSender mailSender;
+	
+	public void sendEmail(String to, String subject, String message){
+		MimeMessage mail = mailSender.createMimeMessage();
+		try {
+            MimeMessageHelper helper = new MimeMessageHelper(mail, true);            
+            helper.setTo(to);
+            helper.setReplyTo(to);
+            helper.setFrom("administrator@invvite.me");
+            helper.setSubject(subject);
+            helper.setText(message);
+            mailSender.send(mail);
+        } catch (MessagingException e) {
+            logger.error("Message not sent "+e.getMessage());
+        }
+	}
+}
