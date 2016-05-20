ca1bf229d4ab39d9b3bff72bfe8eb8d05350f50b
Marcelo Toyoda <marcelohtoy@gmail.com>
2016-05-20 00:51:08 -0300

    Inclusao de classe para gerar senha criptografada

---
+++ src/main/java/br/com/application/security/PasswordManager.java
@@ -0,0 +1,21 @@
+package br.com.application.security;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PasswordManager {
+
+	public static String encrypt(String password) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+		StringBuilder hexString = new StringBuilder();
+		MessageDigest algorithm = MessageDigest.getInstance("SHA-256");
+		byte messageDigest[] = algorithm.digest(password.getBytes("UTF-8"));
+		for (byte b : messageDigest) {
+			hexString.append(String.format("%02X", 0xFF & b));
+		}
+		return hexString.toString();
+	}
+}
